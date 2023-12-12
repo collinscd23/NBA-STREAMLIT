6f6510fd45f0f1ade6c05e09436813ac77d2889c
Carson Collins <113946812+collinscd23@users.noreply.github.com>
2023-12-12 16:06:21 -0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -116,7 +116,7 @@ if selected_player:
 
 # Sample plot: Points per season
 st.write("### Points per Season")
-selected_season = st.selectbox('Select a Season', data['Year'].unique())
+selected_season = st.selectbox('Select a Season', nba_data['Year'].unique())
 filtered_data = data[data['Year'] == selected_season]
 fig = px.bar(filtered_data, x='PLAYER', y='PTS', title=f'Points in Season {selected_season}')
 st.plotly_chart(fig)
